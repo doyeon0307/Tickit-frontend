5be8a1d4644b7b3dbdb63da0cc855f42cc066139
doyeon0307 <eg4rang@naver.com>
2024-10-17 14:40:14 +0900

    feat: splash screen

---
+++ tickit/lib/common/const/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  AppColors._();
+
+  static Color primaryColor = const Color(0xffEEC643);
+  static Color backgroundColor = Colors.white;
+  static Color fillColor = const Color(0xffEEF0F2);
+}
---
+++ tickit/lib/common/screen/splash_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:tickit/common/component/custom_loading.dart';
+import 'package:tickit/common/const/app_colors.dart';
+import 'package:tickit/common/layout/default_layout.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Tickit!",
+              style: TextStyle(
+                color: AppColors.primaryColor,
+                fontWeight: FontWeight.bold,
+                fontSize: 40.0,
+              ),
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            const Text(
+              "내 손으로 만드는 나만의 티켓북",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 16.0,
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            const CustomLoading(),
+          ],
+        ),
+      ),
+    );
+  }
+}
