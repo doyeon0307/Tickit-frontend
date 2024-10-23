1d16df6fd45df9e624166ade575da16cfb7ff0a2
doyeon0307 <eg4rang@naver.com>
2024-10-23 12:29:35 +0900

    feat: 제목,장소 입력 텍스트필드

---
+++ tickit/lib/ui/ticket/component/ticket_text_field.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:tickit/theme/text_style.dart';
+import 'package:tickit/ui/common/const/app_colors.dart';
+
+class TicketTextField extends StatelessWidget {
+  final double fontSize;
+  final String hintText;
+  final TextInputType keyboardType;
+
+  const TicketTextField({
+    super.key,
+    required this.fontSize,
+    required this.hintText,
+    this.keyboardType = TextInputType.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      maxLines: null,
+      keyboardType: keyboardType,
+      style: ticketStyle.copyWith(
+        fontSize: fontSize,
+      ),
+      textAlign: TextAlign.center,
+      cursorColor: AppColors.textColor.withOpacity(0.6),
+      cursorWidth: 1.5,
+      cursorRadius: const Radius.circular(4.0),
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.zero,
+        hintText: hintText,
+        border: InputBorder.none,
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: AppColors.textColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- tickit/lib/ui/ticket/ticket_view.dart
+++ tickit/lib/ui/ticket/ticket_view.dart
@@ -1,11 +1,13 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:tickit/theme/text_style.dart';
 import 'package:tickit/ui/common/component/custom_text_button.dart';
 import 'package:tickit/ui/common/const/app_colors.dart';
+import 'package:tickit/ui/ticket/component/ticket_text_field.dart';
 
 class TicketScreen extends StatefulWidget {
   const TicketScreen({super.key});
@@ -27,7 +29,8 @@ class _TicketScreenState extends State<TicketScreen> {
           children: [
             GestureDetector(
               onTap: () async {
-                XFile? newImage = await imagePicker.pickImage(source: ImageSource.gallery);
+                XFile? newImage =
+                    await imagePicker.pickImage(source: ImageSource.gallery);
                 if (newImage != null) {
                   setState(() {
                     image = newImage;
@@ -45,68 +48,175 @@ class _TicketScreenState extends State<TicketScreen> {
                         style: ticketStyle,
                       )
                     : DecoratedBox(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: FileImage(File(image!.path)),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  child: Container(),
-                ),
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: FileImage(File(image!.path)),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        child: Container(),
+                      ),
               ),
             ),
-            const SizedBox(
-              height: 16.0,
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 12.0,
+                vertical: 16.0,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SvgPicture.asset(
+                    "assets/deco/left_title_deco.svg",
+                  ),
+                  const Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 4.0,
+                      ),
+                      child: TicketTextField(
+                        fontSize: 20.0,
+                        hintText: "제목을 입력하세요",
+                        keyboardType: TextInputType.multiline,
+                      ),
+                    ),
+                  ),
+                  SvgPicture.asset(
+                    "assets/deco/right_title_deco.svg",
+                  ),
+                ],
+              ),
             ),
-            Wrap(
-              spacing: 16.0,
-              children: [
-                CustomTextButton(
-                  label: "필드 추가",
-                  onPressed: () {},
-                  textStyle: ticketStyle,
-                ),
-                CustomTextButton(
-                  label: "배경색 변경",
-                  onPressed: () {},
-                  textStyle: ticketStyle,
-                ),
-                CustomTextButton(
-                  label: "글자색 변경",
-                  onPressed: () {},
-                  textStyle: ticketStyle,
-                ),
-                CustomTextButton(
-                  label: "일정 불러오기",
-                  onPressed: () {},
-                  textStyle: ticketStyle,
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 16.0,
-            ),
-            Wrap(
-              spacing: 16.0,
-              children: [
-                CustomTextButton(
-                  label: "취소하기",
-                  onPressed: () {},
-                  textStyle: ticketStyle,
-                  backgroundColor: AppColors.secondaryColor,
-                  foregroundColor: AppColors.backgroundColor,
-                  width: 100.0,
-                  height: 28.0,
-                ),
-                CustomTextButton(
-                  label: "저장하기",
-                  onPressed: () {},
-                  textStyle: ticketStyle,
-                  backgroundColor: AppColors.primaryColor,
-                  width: 150.0,
-                  height: 28.0,
-                ),
-              ],
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20.0,
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        children: [
+                          SvgPicture.asset("assets/icon/location.svg"),
+                          const SizedBox(
+                            width: 4.0,
+                          ),
+                          const IntrinsicWidth(
+                            child: TicketTextField(
+                              fontSize: 16.0,
+                              hintText: "장소를 입력하세요",
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        width: 8.0,
+                      ),
+                      Row(
+                        children: [
+                          SvgPicture.asset("assets/icon/mini_calendar.svg"),
+                          const SizedBox(
+                            width: 4.0,
+                          ),
+                          Text(
+                            "날짜를 선택하세요",
+                            style: ticketStyle,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  Wrap(
+                    spacing: 16.0,
+                    children: [
+                      CustomTextButton(
+                        label: "필드 추가",
+                        onPressed: () {},
+                        textStyle: ticketStyle,
+                      ),
+                      CustomTextButton(
+                        label: "배경색 변경",
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(
+                                    16.0,
+                                  ),
+                                ),
+                                titleTextStyle: ticketStyle.copyWith(
+                                  fontSize: 24.0,
+                                ),
+                                insetPadding: const EdgeInsets.symmetric(
+                                  horizontal: 8.0,
+                                  vertical: 16.0,
+                                ),
+                                backgroundColor: AppColors.backgroundColor,
+                                title: const Text(
+                                  "배경색을 선택하세요",
+                                ),
+                                content: BlockPicker(
+                                  pickerColor: Colors.white,
+                                  onColorChanged: (value) {},
+                                ),
+                                actions: [
+                                  CustomTextButton(
+                                    label: "선택",
+                                    textStyle: ticketStyle,
+                                    onPressed: () {},
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                        textStyle: ticketStyle,
+                      ),
+                      CustomTextButton(
+                        label: "글자색 변경",
+                        onPressed: () {},
+                        textStyle: ticketStyle,
+                      ),
+                      CustomTextButton(
+                        label: "일정 불러오기",
+                        onPressed: () {},
+                        textStyle: ticketStyle,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 16.0,
+                  ),
+                  Wrap(
+                    spacing: 16.0,
+                    children: [
+                      CustomTextButton(
+                        label: "취소하기",
+                        onPressed: () {},
+                        textStyle: ticketStyle,
+                        backgroundColor: AppColors.secondaryColor,
+                        foregroundColor: AppColors.backgroundColor,
+                        width: 100.0,
+                        height: 28.0,
+                      ),
+                      CustomTextButton(
+                        label: "저장하기",
+                        onPressed: () {},
+                        textStyle: ticketStyle,
+                        backgroundColor: AppColors.primaryColor,
+                        width: 150.0,
+                        height: 28.0,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             )
           ],
         ),
