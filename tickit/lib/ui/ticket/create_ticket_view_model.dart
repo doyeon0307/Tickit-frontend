e122f460aaa247ef4e94fc76ed13ce9cdcd876f9
doyeon0307 <eg4rang@naver.com>
2024-11-17 22:30:38 +0900

    refactor: move directory

--- tickit/lib/ui/ticket/create_ticket_view_model.dart
+++ tickit/lib/ui/ticket/create_ticket_view_model.dart
@@ -1,30 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:tickit/core/loading_status.dart';
 import 'package:tickit/core/use_case/use_case_result.dart';
 import 'package:tickit/domain/s3/get_presigned_url_use_case.dart';
 import 'package:tickit/domain/s3/model/s3_url_model.dart';
 import 'package:tickit/domain/s3/upload_image_to_s3_use_case.dart';
 import 'package:tickit/domain/ticket/create_ticket_use_case.dart';
+import 'package:tickit/ui/ticket/base_ticket_view_model.dart';
 import 'package:tickit/ui/ticket/ticket_state.dart';
 
-final ticketViewModelProvider =
-    StateNotifierProvider.autoDispose<TicketViewModel, TicketState>(
-  (ref) => TicketViewModel(
-    createTicketUseCase: ref.read(createTicketUseCaseProvider),
-    getPresignedUrlUseCase: ref.read(getPresignedUrlUseCaseProvider),
-    uploadImageToS3UseCase: ref.read(uploadImageToS3UseCaseProvider),
-  ),
-);
-
-class TicketViewModel extends StateNotifier<TicketState> {
+class TicketViewModel extends BaseTicketViewModel {
   final CreateTicketUseCase _createTicketUseCase;
   final GetPresignedUrlUseCase _getPresignedUrlUseCase;
   final UploadImageToS3UseCase _uploadImageToS3UseCase;
-
-  final TextEditingController titleController = TextEditingController();
-  final TextEditingController locationController = TextEditingController();
 
   TicketViewModel({
     required CreateTicketUseCase createTicketUseCase,
@@ -32,9 +19,9 @@ class TicketViewModel extends StateNotifier<TicketState> {
     required UploadImageToS3UseCase uploadImageToS3UseCase,
   })  : _createTicketUseCase = createTicketUseCase,
         _getPresignedUrlUseCase = getPresignedUrlUseCase,
-        _uploadImageToS3UseCase = uploadImageToS3UseCase,
-        super(TicketState());
+        _uploadImageToS3UseCase = uploadImageToS3UseCase;
 
+  @override
   void initState() {
     state = TicketState();
     titleController.clear();
@@ -42,111 +29,14 @@ class TicketViewModel extends StateNotifier<TicketState> {
     debugPrint("티켓 UI 초기화 완료");
   }
 
-  void onTapImageBox({
-    required XFile? newImage,
-  }) {
-    if (mounted) {
-      if (newImage != null) {
-        state = state.copyWith(image: newImage);
-        debugPrint("image: ${state.image?.path}");
-      } else {
-        state = state.copyWith(errorMsg: "이미지를 불러올 수 없어요.");
-      }
-    }
-  }
-
-  void onChangedTitle({
-    required newTitle,
-  }) {
-    if (mounted) {
-      state = state.copyWith(title: newTitle);
-      debugPrint("title: ${state.title}");
-    }
-  }
-
-  void onChangedLocation({
-    required String newLocation,
-  }) {
-    if (mounted) {
-      state = state.copyWith(location: newLocation);
-      debugPrint("location: ${state.location}");
-    }
-  }
-
-  void onTapAmButton() {
-    if (mounted) {
-      state = state.copyWith(isAm: !state.isAm);
-      debugPrint("isAm: ${state.isAm}");
-    }
-  }
-
-  void onChangedDate({
-    required DateTime newDate,
-  }) {
-    if (mounted) {
-      state = state.copyWith(date: newDate);
-      debugPrint("date: ${state.date?.toString()}");
-    }
-  }
-
-  void onChangedHour({
-    required int newHour,
-  }) {
-    if (mounted) {
-      state = state.copyWith(hour: newHour);
-      debugPrint("hour: ${state.hour}");
-    }
-  }
-
-  void onChangedMinute({
-    required int newMinute,
-  }) {
-    if (mounted) {
-      state = state.copyWith(minute: newMinute);
-      debugPrint("minute: ${state.minute}");
-    }
-  }
-
-  void onPressedDateTimeCheck() {
-    if (mounted) {
-      state = state.copyWith(
-        date: state.date ?? DateTime.now(),
-      );
-    }
-    final dateTime =
-        "${state.date.toString().split(" ")[0]}  ${state.hour}:${state.minute}";
-    if (mounted) {
-      state = state.copyWith(
-        dateTime: dateTime,
-      );
-      debugPrint("dateTime: ${state.dateTime}");
-    }
-  }
-
-  void onBackgroundColorChanged({
-    required Color newColor,
-  }) {
-    if (mounted) {
-      state = state.copyWith(backgroundColor: newColor);
-      debugPrint("backgroundColor: ${state.backgroundColor}");
-    }
-  }
-
-  void onForegroundColorChanged({
-    required Color newColor,
-  }) {
-    if (mounted) {
-      state = state.copyWith(foregroundColor: newColor);
-      debugPrint("foregroundColor: ${state.foregroundColor}");
-    }
-  }
-
+  @override
   void onPressedCancel() {
     if (mounted) {
       initState();
     }
   }
 
+  @override
   Future<void> onPressedSave() async {
     if (state.image == null ||
         state.dateTime == "날짜를 선택하세요" ||
